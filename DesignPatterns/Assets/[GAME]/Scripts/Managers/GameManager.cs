356108b57a258386c7975f987a3222867150c698
melenglobal <75787400+melenglobal@users.noreply.github.com>
2021-12-06 00:24:46 +0300

    Grammar

--- DesignPatterns/Assets/[GAME]/Scripts/Managers/GameManager.cs
+++ DesignPatterns/Assets/[GAME]/Scripts/Managers/GameManager.cs
@@ -30,20 +30,20 @@ public class GameManager : Singleton<GameManager>
         //Get how many scenes are loaded
         int sceneCount = SceneManager.sceneCount;
         //Create list for scenes to be unloaded
-        List<Scene> scenesToUnLoaded = new List<Scene>();
+        List<Scene> scenesToBeUnLoaded = new List<Scene>();
         //Add scenes to be unloaded tothe list
         for (int i = 0; i < sceneCount; i++)
         {
             Scene scene = SceneManager.GetSceneAt(i);
             if (scene.name.Contains("Level"))
             {
-                scenesToUnLoaded.Add(scene);
+                scenesToBeUnLoaded.Add(scene);
             }
         }
         //Unload all scenes that needs to be unloaded
-        foreach (var s in scenesToUnLoaded)
+        foreach (var s in scenesToBeUnLoaded)
         {
-            yield return SceneManager.UnloadSceneAsync(s);
+            yield return SceneManager.UnloadSceneAsync(s.buildIndex);
         }
         //Check if we can load  this scene
         if (!Application.CanStreamedLevelBeLoaded(buildIndex))
@@ -53,7 +53,7 @@ public class GameManager : Singleton<GameManager>
         }
         //Load the scene
         yield return SceneManager.LoadSceneAsync(buildIndex, LoadSceneMode.Additive);
-        Scene levelScene = SceneManager.GetSceneAt(1);
+        Scene levelScene = SceneManager.GetSceneByBuildIndex(buildIndex);
         SceneManager.SetActiveScene(levelScene);
         PlayerPrefs.SetString("LastLevel",levelScene.name);
     }
